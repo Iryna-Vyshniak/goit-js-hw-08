ed28b6295f1e8b4dbddc5847e0aa9ddbfd1b0803
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-20 00:18:41 +0200

    add next variant to player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,33 +1,62 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+// --------------- 1st variant --------------------
 
-const onPlay = function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// const onPlay = function (data) {
+//     localStorage.setItem('videoplayer-current-time', data.seconds);
+// };
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+// player.setCurrentTime(currentTime).then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function (error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
+
+// player.setColor('#45a247').then(function (color) {
+//     // the color that was set
+// }).catch(function (error) {
+//     // an error occurred setting the color
+// });
+
+// --------------- 2nd variant --------------------
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
 };
 
-player.on('timeupdate', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(getCurrentTime, 1000));
 
-const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
 
-player.setCurrentTime(currentTime).then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function (error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-        default:
-            // some other error occurred
-            break;
-    }
-});
-
-player.setColor('#45a247').then(function (color) {
-    // the color that was set
-}).catch(function (error) {
-    // an error occurred setting the color
-});
-
+player
+  .setColor('#d8e0ff')
+  .then(function (color) {
+    console.log('The new color value: #D8E0FF');
+  })
+  .catch(function (error) {
+    console.log('An error occurred while setting the color');
+  });
